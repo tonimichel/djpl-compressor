04f918c827e999510c36209ca51323ea6f342459
Toni Michel <toni.michel@schnapptack.de>
2015-08-14 12:25:01 +0200

    added less processing; adjusted media_inlcudes to compress block authenticated js

--- feature_compressor/settings.py
+++ feature_compressor/settings.py
@@ -2,9 +2,13 @@
 
 def refine_INSTALLED_APPS(original):
     return  ['compressor', 'feature_compressor'] + list(original)
-    
-    
+
+
 def refine_STATICFILES_FINDERS(original):
     return list(original) + ['compressor.finders.CompressorFinder',]
-    
+
 introduce_COMPRESS_JS_FILTERS = ['feature_compressor.jsfilters.SemicolonAppender']
+
+introduce_COMPRESS_PRECOMPILERS = (
+    ('text/less', 'lessc {infile} {outfile}'),
+)
